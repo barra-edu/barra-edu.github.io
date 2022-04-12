56bce914b4cd868f1575953c01f278766f7054ab
russell-moro-bp <99088161+russell-moro-bp@users.noreply.github.com>
2022-04-12 11:00:42 +0100

    Create es4.h

---
+++ 4ca/es4.h
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse; /* default: separate */
+  }
+</style>  
+</head>
+  <body>
+    <table>
+      <tr>
+        <th rowspan="2">N.</th>
+        <td rowspan="2"titolo></td>
+        <th colspan="2">data ......</th>
+        <!-- <td></td> -->
+      </tr>
+      <tr>
+        <!-- <td></td> -->
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>  
+  </body>
+</html>
